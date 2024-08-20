fa2d66980c116f8f314af12557b4be8681f59127
Ayushkumar025 <ayushkumar0117@gmail.com>
2024-08-20 21:24:33 +0530

    js-practice

---
+++ script.js
@@ -0,0 +1,58 @@
+// Addition
+let a=7;
+let b=3;
+let sum=a+b;
+console.log("sum = "+sum);
+
+/*Subtraction */ 
+let c=15;
+let d=8;
+let difference=c-d;
+console.log("difference = "+difference);
+
+// product
+let e=6;
+let f=4;
+let product=e*f;
+console.log("product = "+product);
+
+// divide
+let g=20;
+let h=4;
+let quotient=g/h;
+console.log("quotient = "+quotient);
+
+// remainder
+let i=10;
+let j=3;
+let remainder=10%3;
+console.log("remainder = "+remainder);
+
+// add first two then multiply with third
+let k=2;
+let l=3;
+let m=4;
+let add=k+l;
+let mul=add*m;
+console.log("result = "+mul);
+
+// average
+let n=5;
+let o=10;
+let p=15;
+let Addition=n+o+p;
+let average=Addition/3;
+console.log("average = "+average);
+
+// increment
+let q=7;
+q++;
+let r=5;
+let total=q*r;
+console.log("Total = "+total);
+
+
+
+
+
+
